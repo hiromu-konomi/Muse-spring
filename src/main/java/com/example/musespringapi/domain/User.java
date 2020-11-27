4e445fb4a3c5bbf4928cdbd079e546f48357596f
konomihiromu <hiromu.konomi@rakus-partners.co.jp>
2020-11-27 18:34:38 +0900

    画像のフィールド変数の型をbyte[]に変更

--- src/main/java/com/example/musespringapi/domain/User.java
+++ src/main/java/com/example/musespringapi/domain/User.java
@@ -1,9 +1,12 @@
 package com.example.musespringapi.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -13,22 +16,37 @@ import java.util.Date;
 @Entity
 @Setter
 @Getter
+@Table(name="users")
 public class User {
 
     /** 自動採番ID */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
+
     /** ユーザー名 */
+    @Column(name = "user_name", columnDefinition = "VARCHAR(45)")
     private String userName;
+
     /** プロフィール画像 */
-    private String profilePicture;
+    @Lob
+    @Column(name = "profile_picture", columnDefinition = "BLOB")
+    private byte[] profilePicture;
+
     /** ヘッダー画像 */
-    private String profileHeadPicture;
+    @Lob
+    @Column(name = "profile_head_picture", columnDefinition = "BLOB")
+    private byte[] profileHeadPicture;
+
     /** プロフィール */
+    @Column(name = "profile", columnDefinition = "VARCHAR(255)")
     private String profile;
+
     /** 部署ID */
+    @Column(name = "dep_id", columnDefinition = "INT")
     private Integer depId;
+
     /** 入社年月 */
+    @Column(name = "hire_date", columnDefinition = "DATE")
     private Date hireDate;
 }
