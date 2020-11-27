f1cb5d2a6628398949edb83f21e958c4ec562f5a
konomihiromu <hiromu.konomi@rakus-partners.co.jp>
2020-11-27 11:26:52 +0900

    init

---
+++ src/main/java/com/example/musespringapi/MuseSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.musespringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuseSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuseSpringApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/musespringapi/MuseSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.musespringapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuseSpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
