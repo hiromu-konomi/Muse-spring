7a44b58b40035dbe2117d091aa848e069bd263ea
澤口ありさ <sawaguchiarisa@sawaguchiarisanoMacBook-Pro.local>
2020-12-04 10:53:16 +0900

    Repository追加

---
+++ src/main/java/com/example/musespringapi/repository/FirebaseRepository.java
@@ -0,0 +1,14 @@
+package com.example.musespringapi.repository;
+
+import java.util.List;
+
+import com.example.musespringapi.domain.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FirebaseRepository extends JpaRepository<User, String> {
+
+    List<User> findByUserNum(String userNum);
+}
