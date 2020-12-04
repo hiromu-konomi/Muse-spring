166d40bbc32c56840f9def5b24e795e679aa7574
澤口ありさ <sawaguchiarisa@sawaguchiarisanoMacBook-Pro.local>
2020-12-04 10:52:15 +0900

    UserController追加

--- src/main/java/com/example/musespringapi/controller/UserController.java
+++ src/main/java/com/example/musespringapi/controller/UserController.java
@@ -1,27 +1,48 @@
 package com.example.musespringapi.controller;
 
+import com.example.musespringapi.domain.User;
 import com.example.musespringapi.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @CrossOrigin
 public class UserController {
-    
-    @Autowired
-    UserService userService;
 
-   @PostMapping("/users")
-   @ResponseStatus(HttpStatus.CREATED)
-    public void userPost(@RequestBody String userId) {
-	   System.out.println("userId="+ userId);
-	   
-	   userService.insertUserId(userId);
-    }
+  @Autowired
+  UserService userService;
+
+  @PostMapping("/users")
+  @ResponseStatus(HttpStatus.CREATED)
+  public void userPost(@RequestBody String userNum) {
+    System.out.println("userId=" + userNum);
+
+    userService.insertUserId(userNum);
+  }
+
+  // @RequestMapping(path = "/postform", method = RequestMethod.GET)
+  @GetMapping("/postform")
+  public String show(String userNum) {
+    System.out.println(userNum);
+    List<User> userList = userService.findByUserNum(userNum);
+
+    User user = new User();
+    user.setUserName(userList.get(0).getUserName());
+    System.out.println(user.getUserName());
+    return user.getUserName();
+
+  }
 }
