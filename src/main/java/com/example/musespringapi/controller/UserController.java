2595bee43f84a730d8fc382ae97af459695d2bc5
kajiyama-miyu <miyu.kajiyama@rakus-partners.co.jp>
2020-12-04 11:13:03 +0900

    ユーザー詳細情報取得処理

--- src/main/java/com/example/musespringapi/controller/UserController.java
+++ src/main/java/com/example/musespringapi/controller/UserController.java
@@ -1,10 +1,15 @@
 package com.example.musespringapi.controller;
 
+import com.example.musespringapi.domain.User;
 import com.example.musespringapi.service.UserService;
+
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -19,9 +24,25 @@ public class UserController {
 
    @PostMapping("/users")
    @ResponseStatus(HttpStatus.CREATED)
-    public void userPost(@RequestBody String userId) {
+    public void userPost(String userId) {
 	   System.out.println("userId="+ userId);
 	   
 	   userService.insertUserId(userId);
     }
+   
+   @GetMapping("/users")
+   public User findByUserId(String userId){
+	   System.out.println(userId);
+	   User userDetailbyId = userService.findByUserId(userId);
+	  
+	   return userDetailbyId; 
+	   
+   }
+   
+   @PostMapping("/userDetail")
+   @ResponseStatus(HttpStatus.CREATED)
+   public void addUserDetail (User user) {
+	   System.out.println(user);
+	   userService.insertUser(user);
+   }
 }
