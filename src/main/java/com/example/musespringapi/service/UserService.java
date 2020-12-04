21e95ab5041406bc3be9e7dad2309049e74682c9
澤口ありさ <sawaguchiarisa@sawaguchiarisanoMacBook-Pro.local>
2020-12-04 10:54:01 +0900

    処理追加

--- src/main/java/com/example/musespringapi/service/UserService.java
+++ src/main/java/com/example/musespringapi/service/UserService.java
@@ -1,25 +1,29 @@
 package com.example.musespringapi.service;
 
-import javax.transaction.Transactional;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+
 import com.example.musespringapi.domain.User;
+import com.example.musespringapi.repository.FirebaseRepository;
 import com.example.musespringapi.repository.UserRepository;
 
-
+import java.util.List;
 
 @Service
-@Transactional
+@RequiredArgsConstructor
 public class UserService {
 
-	@Autowired
-	private UserRepository userRepository;
-	
-	public void insertUserId(String userId) {
+	private final UserRepository userRepository;
+	private final FirebaseRepository firebaseRepository;
+
+	public void insertUserId(String userNum) {
 		User user = new User();
-		user.setUserId(userId);
+		user.setUserNum(userNum);
 		userRepository.save(user);
+	}
+
+	public List<User> findByUserNum(String userNum) {
+		return firebaseRepository.findByUserNum(userNum);
 	}
 }
