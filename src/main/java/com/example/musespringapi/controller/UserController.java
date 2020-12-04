cea78508e22bf58dc9b04ccd9a67273a71b0afe0
tatsuki-Kasa <tatsuki.kasawaki@rakus-partners.co.jp>
2020-12-04 09:39:25 +0900

    作業中のためプルができなかったからコード合わせるための変更

--- src/main/java/com/example/musespringapi/controller/UserController.java
+++ src/main/java/com/example/musespringapi/controller/UserController.java
@@ -2,26 +2,25 @@ package com.example.musespringapi.controller;
 
 import com.example.musespringapi.service.UserService;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@CrossOrigin
-public class UserController {
-    
-    @Autowired
-    UserService userService;
+import lombok.RequiredArgsConstructor;
 
-   @PostMapping("/users")
-   @ResponseStatus(HttpStatus.CREATED)
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/users")
+    @ResponseStatus(HttpStatus.CREATED)
     public void userPost(@RequestBody String userId) {
-	   System.out.println("userId="+ userId);
-	   
-	   userService.insertUserId(userId);
+        System.out.println("userId="+ userId);
+
+        userService.insertUserId(userId);
     }
 }
