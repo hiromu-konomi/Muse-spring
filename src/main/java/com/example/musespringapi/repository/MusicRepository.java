4acc33f36a72be67419451f96afbd7133469293c
tatsuki-Kasa <tatsuki.kasawaki@rakus-partners.co.jp>
2020-12-04 09:45:54 +0900

    music repositoryの追加

---
+++ src/main/java/com/example/musespringapi/repository/MusicRepository.java
@@ -0,0 +1,11 @@
+package com.example.musespringapi.repository;
+
+import com.example.musespringapi.domain.Music;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MusicRepository extends JpaRepository<Music, Integer> {
+
+}
