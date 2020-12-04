e3610e3cac33e3b3ed8e87e71678ce12202c1b6e
tatsuki-Kasa <tatsuki.kasawaki@rakus-partners.co.jp>
2020-12-04 09:43:46 +0900

    postテーブルと結合するための処理追加（未完成）

--- src/main/java/com/example/musespringapi/domain/User.java
+++ src/main/java/com/example/musespringapi/domain/User.java
@@ -2,15 +2,18 @@ package com.example.musespringapi.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Setter
@@ -44,4 +47,9 @@ public class User {
     /** 入社年月 */
     @Column(name = "hire_date", columnDefinition = "DATE")
     private Date hireDate;
+
+    /** ポストテーブルとの結合 */
+    // @OneToMany(fetch = FetchType.EAGER, mappedBy = "user")
+    // private List<Post> postListWhomUser;
+
 }
