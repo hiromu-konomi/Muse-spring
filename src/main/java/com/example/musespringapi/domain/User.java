c10b09b87a0ff33116996862d2f41281c7817079
澤口ありさ <sawaguchiarisa@sawaguchiarisanoMacBook-Pro.local>
2020-12-08 15:53:28 +0900

    User追加

--- src/main/java/com/example/musespringapi/domain/User.java
+++ src/main/java/com/example/musespringapi/domain/User.java
@@ -2,18 +2,15 @@ package com.example.musespringapi.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.Date;
-import java.util.List;
 
 @Entity
 @Setter
