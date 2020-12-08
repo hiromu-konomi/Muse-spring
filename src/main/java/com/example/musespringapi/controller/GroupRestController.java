c8cbf63fef7224a51bb046eaa4f44cab61da3b1b
konomihiromu <hiromu.konomi@rakus-partners.co.jp>
2020-12-08 11:44:32 +0900

    グループを新規作成したら管理者のIDをgroupsとgroup_memberのテーブルにINSERTする処理を追加【GroupRestController.java】

--- src/main/java/com/example/musespringapi/controller/GroupRestController.java
+++ src/main/java/com/example/musespringapi/controller/GroupRestController.java
@@ -1,6 +1,9 @@
 package com.example.musespringapi.controller;
 
+import com.example.musespringapi.domain.Group;
+import com.example.musespringapi.domain.GroupMember;
 import com.example.musespringapi.request.GroupRequest;
+import com.example.musespringapi.service.GroupMemberService;
 import com.example.musespringapi.service.GroupService;
 
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,9 +18,22 @@ import lombok.RequiredArgsConstructor;
 public class GroupRestController {
 
     private final GroupService groupService;
+    private final GroupMemberService groupMemberService;
 
     @RequestMapping(value = "/createGroup", method = RequestMethod.POST)
     public void createGroup(@RequestBody GroupRequest request) {
-        groupService.save(request.getGroupName());
+        
+        // groups テーブルに INSERT
+        Group group = new Group();
+        group.setGroupName(request.getGroupName());
+        group.setOwnerUserId(request.getFirebaseId());
+        groupService.save(group);
+
+        // group_id を取得して group_member テーブルに INSERT(管理者のみ)
+        Long groupId = groupService.newGroupId();
+        GroupMember groupMember = new GroupMember();
+        groupMember.setGroupId(groupId);
+        groupMember.setUserNum(request.getFirebaseId());
+        groupMemberService.save(groupMember);
     }
 }
