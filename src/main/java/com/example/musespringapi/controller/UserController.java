03aff7de56f4f09be33e6d187b6e09f8314fbfe0
澤口ありさ <sawaguchiarisa@sawaguchiarisanoMacBook-Pro.local>
2020-12-08 15:52:18 +0900

    途中まで

--- src/main/java/com/example/musespringapi/controller/UserController.java
+++ src/main/java/com/example/musespringapi/controller/UserController.java
@@ -3,22 +3,14 @@ package com.example.musespringapi.controller;
 import com.example.musespringapi.domain.User;
 import com.example.musespringapi.service.UserService;
 
-
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.util.List;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import lombok.RequiredArgsConstructor;
 
@@ -27,27 +19,26 @@ import lombok.RequiredArgsConstructor;
 public class UserController {
 
   private final UserService userService;
-   
-   @GetMapping("/users")
-   public User findByUserId(String userId){
-	   System.out.println(userId);
-	   User userDetailbyId = userService.findByUserId(userId);
-	  
-	   return userDetailbyId; 
-	   
-   }
-   
-   @PostMapping("/userDetail")
-   @ResponseStatus(HttpStatus.CREATED)
-   public void addUserDetail (User user) {
-	   System.out.println(user);
-	   userService.insertUser(user);
-   }
+
+  @GetMapping("/users")
+  public User findByUserId(String userId) {
+    System.out.println(userId);
+    User userDetailbyId = userService.findByUserId(userId);
+
+    return userDetailbyId;
+
+  }
+
+  @PostMapping("/userDetail")
+  @ResponseStatus(HttpStatus.CREATED)
+  public void addUserDetail(User user) {
+
+    userService.insertUser(user);
+  }
 
   @PostMapping("/users")
   @ResponseStatus(HttpStatus.CREATED)
   public void userPost(@RequestBody String userNum) {
-    System.out.println("userId=" + userNum);
 
     userService.insertUserId(userNum);
   }
@@ -55,7 +46,7 @@ public class UserController {
   // @RequestMapping(path = "/postform", method = RequestMethod.GET)
   @GetMapping("/postform")
   public String show(String userNum) {
-    System.out.println(userNum);
+
     List<User> userList = userService.findByUserNum(userNum);
 
     User user = new User();
