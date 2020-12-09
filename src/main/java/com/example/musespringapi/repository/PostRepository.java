7c2517230807037dd71e7ab6fbcb748ff1379f3d
澤口ありさ <sawaguchiarisa@sawaguchiarisanoMacBook-Pro.local>
2020-12-09 15:53:37 +0900

    投稿機能

--- src/main/java/com/example/musespringapi/repository/PostRepository.java
+++ src/main/java/com/example/musespringapi/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.example.musespringapi.repository;
 
+import java.util.List;
+
 import com.example.musespringapi.domain.Post;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +10,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
 
+    List<Post> findByUserNum(String userNum);
 }
