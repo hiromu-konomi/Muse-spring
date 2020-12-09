fecf651d256fa16b4863fe0e4230a2b6d80391d9
tatsuki-Kasa <tatsuki.kasawaki@rakus-partners.co.jp>
2020-12-09 16:23:27 +0900

    @ManyToOneの追加(users,musics)

--- src/main/java/com/example/musespringapi/domain/Post.java
+++ src/main/java/com/example/musespringapi/domain/Post.java
@@ -36,13 +36,15 @@ public class Post {
     private String postText;
 
     // /** ユーザーテーブルとの結合 */
-    // @ManyToOne
-    // @JoinColumn(name = "posts.user_id", referencedColumnName = "users.user_id", insertable = true, updatable = false)
-    // private User user;
+    @ManyToOne
+    @JoinColumn(name = "user_id", insertable = false, updatable = false)
+    private User user;
 
     // /** ミュージックテーブルとの結合 */
-    // @ManyToOne
-    // @JoinColumn(name = "posts.music_id", referencedColumnName = "musics.music_id", insertable = true, updatable = false)
-    // private Music music;
+    // ,referencedColumnName = "musics.music_id", insertable = true, updatable = false
+    @ManyToOne
+    @JoinColumn(name = "music_id", insertable = false, updatable = false)
+    private Music music;
+
 
 }
