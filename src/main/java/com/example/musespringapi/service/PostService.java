377b835054aa52866c8ae685f0d928cde494f894
tatsuki-Kasa <tatsuki.kasawaki@rakus-partners.co.jp>
2020-12-09 16:24:44 +0900

    結合のユーザー、ミュージック、ポスト情報の取得操作（コメントアウト）

--- src/main/java/com/example/musespringapi/service/PostService.java
+++ src/main/java/com/example/musespringapi/service/PostService.java
@@ -1,5 +1,9 @@
 package com.example.musespringapi.service;
 
+import java.util.List;
+
+import javax.persistence.EntityManagerFactory;
+
 import com.example.musespringapi.domain.Music;
 import com.example.musespringapi.domain.Post;
 import com.example.musespringapi.repository.MusicRepository;
@@ -30,5 +34,19 @@ public class PostService {
         musicRepository.save(music);
 
     }
+
+    // public List<Post> selectPost(String userId) {
+
+    //     EntityManagerFactory emf = P
+
+    //     List<Post> postLists = entityManager
+    //     .createNativeQuery("SELECT artist_name, music_name, post_text"
+    //     + "FROM posts p INNER JOIN musics m ON p.music_id = m.music_id"
+    //     + "INNER JOIN users u ON p.user_id = u.user_id"
+    //     + "WHERE user_id = :userId", Post.class)
+    //     .setParameter("userId", userId)
+    //     .getResultList();
+
+    // }
 
 }
