a91a5a3e8283c184b78d9830ee1cd3910868adcf
tatsuki-Kasa <tatsuki.kasawaki@rakus-partners.co.jp>
2020-12-09 16:39:41 +0900

    import文の追加

--- src/main/java/com/example/musespringapi/domain/User.java
+++ src/main/java/com/example/musespringapi/domain/User.java
@@ -5,12 +5,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Setter
