36169570ad80fb75b784b1cbddc6451ddf89b605
澤口ありさ <sawaguchiarisa@sawaguchiarisanoMacBook-Pro.local>
2020-12-11 17:19:09 +0900

    MusicResponce追加

---
+++ src/main/java/com/example/musespringapi/response/MusicResponce.java
@@ -0,0 +1,15 @@
+package com.example.musespringapi.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MusicResponce {
+
+    private String artistName;
+
+    private String musicName;
+
+    private String image;
+}
