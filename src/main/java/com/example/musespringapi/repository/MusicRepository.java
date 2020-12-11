1e2a240595afb34fdf9b8872c5522e28a962e460
tatsuki-Kasa <tatsuki.kasawaki@rakus-partners.co.jp>
2020-12-11 17:03:08 +0900

    アーティスト名であいまい検索するためのレポジトリ

--- src/main/java/com/example/musespringapi/repository/MusicRepository.java
+++ src/main/java/com/example/musespringapi/repository/MusicRepository.java
@@ -1,11 +1,18 @@
 package com.example.musespringapi.repository;
 
+import java.util.List;
+
 import com.example.musespringapi.domain.Music;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MusicRepository extends JpaRepository<Music, Integer> {
+
+    //musicNameをもとにmusicId,MusicName,artistNameを検索
+    @Query(value = "SELECT * FROM `musics` WHERE `artist_name` LIKE %?1%", nativeQuery = true)
+    List<Music> findByArtistNameStartsWith(String searchPost);
 
 }
