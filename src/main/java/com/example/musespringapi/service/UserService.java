b2794c83da3aa77979eae113b2912b4027bd173d
kajiyama-miyu <miyu.kajiyama@rakus-partners.co.jp>
2020-12-11 17:22:30 +0900

    UserService

--- src/main/java/com/example/musespringapi/service/UserService.java
+++ src/main/java/com/example/musespringapi/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.musespringapi.service;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -40,5 +41,10 @@ public class UserService {
 
 	public List<User> findByUserNum(String userNum) {
 		return firebaseRepository.findByUserNum(userNum);
+	}
+	
+	//入社日が同じ日のユーザー検索
+	public List<User> findByHireDate(Date hireDate){
+		return userRepository.findByHireDate(hireDate);
 	}
 }
