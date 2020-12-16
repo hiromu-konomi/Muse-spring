7ff4aa1cd9ee4141d6f84ce40db35fee0a1e3698
澤口ありさ <sawaguchiarisa@sawaguchiarisanoMacBook-Pro.local>
2020-12-16 14:48:54 +0900

    メソッド追加

--- src/main/java/com/example/musespringapi/repository/PostRepository.java
+++ src/main/java/com/example/musespringapi/repository/PostRepository.java
@@ -11,11 +11,15 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
 
+    // userNumから検索する処理
     List<Post> findByUserNum(String userNum);
 
-    Post findByPostId(Integer postId);
+    // postIdから検索する処理
+    @Query(value = "SELECT * FROM `posts` WHERE `post_id` = ?1 ORDER BY `post_id` DESC", nativeQuery = true)
+    List<Post> findByPostId(Integer postId);
 
-    @Query(value = "SELECT `post_id` FROM `posts` WHERE `user_num` = ?1", nativeQuery = true)
-    List<Integer> getPostIdFromFollowingUser(String followingUser);
+    // フォローしてるpostIDを取ってくる処理
+    @Query(value = "SELECT * FROM `posts` WHERE `user_num` = ?1 ORDER BY `post_id` DESC", nativeQuery = true)
+    List<Post> getPostIdFromFollowingUser(String followingUser);
 
 }
