b5aa14f1f5b82c57341c98f65e0c7eb17c7dcd5d
澤口ありさ <sawaguchiarisa@sawaguchiarisanoMacBook-Pro.local>
2020-12-16 14:47:17 +0900

    ホーム画面作成完了

--- src/main/java/com/example/musespringapi/controller/HomeRestController.java
+++ src/main/java/com/example/musespringapi/controller/HomeRestController.java
@@ -1,22 +1,26 @@
 package com.example.musespringapi.controller;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collector;
 
 import com.example.musespringapi.domain.Music;
-import com.example.musespringapi.response.MusicResponce;
+import com.example.musespringapi.domain.Post;
+import com.example.musespringapi.domain.ShowReview;
+import com.example.musespringapi.domain.User;
 import com.example.musespringapi.response.PostResponce;
 import com.example.musespringapi.service.PostCardService;
 import com.example.musespringapi.service.PostService;
 import com.example.musespringapi.service.RelationService;
+import com.example.musespringapi.service.UserService;
 
+import org.hibernate.engine.internal.Collections;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javassist.expr.NewArray;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -26,33 +30,39 @@ public class HomeRestController {
     private final PostCardService postCardService;
     private final RelationService relationService;
     private final PostService postService;
+    private final UserService userService;
 
-    @GetMapping("/getMusicInfo")
-    public MusicResponce getMusicInfo(Integer postId) {
-        Music music = postCardService.findByPostId(postId);
-        MusicResponce musicResponce = new MusicResponce();
-        musicResponce.setArtistName(music.getArtistName());
-        musicResponce.setMusicName(music.getMusicName());
-        musicResponce.setImage(music.getImage());
-        System.out.println(musicResponce.getImage());
-        return musicResponce;
-    }
-
-    //
-    @GetMapping("/getFollowingUser")
-    public ResponseEntity<PostResponce> getFollowingUserNum(String userNum) {
+    @GetMapping("/getMusicInfoAndReview")
+    public ResponseEntity<PostResponce> getMusicInfo(String userNum) {
         List<String> followingUserList = relationService.getFollowingUserNum(userNum);
-        List<Integer> postIdAllList = new ArrayList<>();
+        followingUserList.add(userNum);
+        List<ShowReview> reviewList = new ArrayList<>();
+        List<Post> postIdAllList = new ArrayList<>();
         for (String followingUserNum : followingUserList) {
-            List<Integer> postIdList = postService.getPostIdFromFollowingUser(followingUserNum);
-            for (Integer postId : postIdList) {
-                postIdAllList.add(postId);
+            List<Post> postIdList = postService.getPostIdFromFollowingUser(followingUserNum);
+            for (Post post : postIdList) {
+                postIdAllList.add(post);
             }
         }
-        postIdAllList.sort(Comparator.reverseOrder());
-        PostResponce postResponce = PostResponce.builder().postIdAllList(postIdAllList).build();
+        for (Post post : postIdAllList) {
+            ShowReview showReview = new ShowReview();
+            Music music = postCardService.findByPostId(post.getPostId());
+            showReview.setArtistName(music.getArtistName());
+            showReview.setMusicImage(music.getImage());
+            showReview.setMusicName(music.getMusicName());
+            User user = userService.userName(post.getUserNum());
+            showReview.setUserName(user.getUserName());
+            showReview.setPostText(post.getPostText());
+            showReview.setPostId(post.getPostId());
+            reviewList.add(showReview);
+        }
 
+        reviewList.sort(Comparator.comparing(ShowReview::getPostId).reversed());
+
+        PostResponce postResponce = PostResponce.builder().reviewAllList(reviewList).build();
+        System.out.println(postResponce.getReviewAllList().size());
         return new ResponseEntity<>(postResponce, HttpStatus.OK);
+
     }
 
 }
