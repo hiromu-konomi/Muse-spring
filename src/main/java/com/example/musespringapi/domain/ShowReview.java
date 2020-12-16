98aa941ad2942cd0db3c6eefe2a1d89b4150247a
澤口ありさ <sawaguchiarisa@sawaguchiarisanoMacBook-Pro.local>
2020-12-16 14:53:21 +0900

    ShowReviewドメイン追加

---
+++ src/main/java/com/example/musespringapi/domain/ShowReview.java
@@ -0,0 +1,31 @@
+package com.example.musespringapi.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class ShowReview {
+
+    // ** アーティストネーム */
+    private String artistName;
+
+    /** 曲名 */
+    private String musicName;
+
+    /** ジャケ写 */
+    private String musicImage;
+
+    /** 投稿テキスト */
+    private String postText;
+
+    /** Post Id */
+    private Integer postId;
+
+    private String userName;
+
+    int getPosrId() {
+        return this.postId;
+    }
+
+}
