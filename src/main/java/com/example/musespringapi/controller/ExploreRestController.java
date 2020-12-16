8d491ea4f68195d7de419622a9d7e8f3ac7f67a1
澤口ありさ <sawaguchiarisa@sawaguchiarisanoMacBook-Pro.local>
2020-12-16 15:08:43 +0900

    エラー修正

--- src/main/java/com/example/musespringapi/controller/ExploreRestController.java
+++ src/main/java/com/example/musespringapi/controller/ExploreRestController.java
@@ -2,10 +2,9 @@ package com.example.musespringapi.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import com.example.musespringapi.domain.Explore;
 import com.example.musespringapi.domain.Music;
 import com.example.musespringapi.domain.Post;
+import com.example.musespringapi.domain.ShowReview;
 import com.example.musespringapi.domain.User;
 import com.example.musespringapi.response.MusicForExploreResponse;
 import com.example.musespringapi.service.ExploreService;
@@ -25,35 +24,36 @@ public class ExploreRestController {
     private final ExploreService exploreService;
 
     @RequestMapping(value = "/searchPost", method = RequestMethod.GET)
-    public ResponseEntity<MusicForExploreResponse> SelectPost(String searchPost){
+    public ResponseEntity<MusicForExploreResponse> SelectPost(String searchPost) {
 
         List<Music> musicList = exploreService.postIdFindByMusicName(searchPost);
-        List<Explore> exploreList = new ArrayList<>();
+        List<ShowReview> exploreList = new ArrayList<>();
 
-        for(Music music : musicList){
+        for (Music music : musicList) {
 
-            Explore explore = new Explore();
+            ShowReview showReview = new ShowReview();
 
-            explore.setArtistName(music.getArtistName());
-            explore.setMusicName(music.getMusicName());
-            explore.setMusicImage(music.getImage());
+            showReview.setArtistName(music.getArtistName());
+            showReview.setMusicName(music.getMusicName());
+            showReview.setMusicImage(music.getImage());
 
             Post post = exploreService.userNumPostTextFindByPostId(music.getPostId());
 
-            explore.setPostText(post.getPostText());
+            showReview.setPostText(post.getPostText());
 
             Integer userIntegerNum = Integer.valueOf(post.getUserNum());
             User user = exploreService.userNameFindByUserNum(userIntegerNum);
 
-            explore.setUserName(user.getUserName());
+            showReview.setUserName(user.getUserName());
 
-            exploreList.add(explore);
+            exploreList.add(showReview);
 
             // System.out.println("test");
 
         }
 
-        MusicForExploreResponse musicForExploreResponse = MusicForExploreResponse.builder().exploreList(exploreList).build();
+        MusicForExploreResponse musicForExploreResponse = MusicForExploreResponse.builder().exploreList(exploreList)
+                .build();
         // System.out.println(musicForExploreResponse.getExploreList().get(0).getArtistName());
         return new ResponseEntity<>(musicForExploreResponse, HttpStatus.OK);
 
