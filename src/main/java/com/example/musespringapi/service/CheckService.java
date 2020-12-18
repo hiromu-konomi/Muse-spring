4dd94b5835f92db982639445223c0f004a566d9d
kajiyama-miyu <miyu.kajiyama@rakus-partners.co.jp>
2020-12-18 20:19:52 +0900

    チェックした投稿

--- src/main/java/com/example/musespringapi/service/CheckService.java
+++ src/main/java/com/example/musespringapi/service/CheckService.java
@@ -1,7 +1,13 @@
 package com.example.musespringapi.service;
 
 import com.example.musespringapi.domain.Check;
+import com.example.musespringapi.domain.Music;
 import com.example.musespringapi.repository.CheckRepository;
+import com.example.musespringapi.repository.MusicRepository;
+
+import java.util.List;
+
+
 
 import org.springframework.stereotype.Service;
 
@@ -12,6 +18,7 @@ import lombok.RequiredArgsConstructor;
 public class CheckService {
 
     private final CheckRepository checkRepository;
+    private final MusicRepository musicRepository;
 
     public void insertChecks(Check check) {
         checkRepository.save(check);
@@ -31,5 +38,24 @@ public class CheckService {
 
     public void deleteChecks(Integer musicId, String userNum) {
         checkRepository.deleteChecks(musicId, userNum);
+    }
+    
+    public List<Check> getCheckSongsByUserNum(String userNum){
+    	List<Check> checkList = checkRepository.findByUserNum(userNum);
+    	
+    	if(checkList.isEmpty()) {
+    		return null;
+    	}
+    	
+    	return checkList;
+    }
+    
+    public Music getMusicsByMusicId(Integer musicId){
+    	List<Music> musicList = musicRepository.findByMusicId(musicId);
+    	
+    	if(musicList.isEmpty()) {
+    		return null;
+    	}
+    	return musicList.get(0);
     }
 }
