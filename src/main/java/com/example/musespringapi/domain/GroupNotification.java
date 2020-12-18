f11d4f22496e3b85c297eacf931fd9bf10c8cd00
tatsuki-Kasa <tatsuki.kasawaki@rakus-partners.co.jp>
2020-12-18 15:11:02 +0900

    GroupNotificationの追加

---
+++ src/main/java/com/example/musespringapi/domain/GroupNotification.java
@@ -0,0 +1,32 @@
+package com.example.musespringapi.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "group_notification")
+public class GroupNotification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer groupNotificationId;
+
+    @Column(name = "group_transfer", columnDefinition = "VARCHAR(225)")
+    private String groupTransfer;
+
+    @Column(name = "group_receiver", columnDefinition = "VARCHAR(225)")
+    private String groupReceiver;
+
+    @Column(name = "notification_parents_id")
+    private Integer notificationParentsId;
+
+}
