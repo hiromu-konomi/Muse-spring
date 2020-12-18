31883b3ac88eed0ccd9045f355f7eaee9514c90a
tatsuki-Kasa <tatsuki.kasawaki@rakus-partners.co.jp>
2020-12-18 15:09:26 +0900

    follow_notificationにインサート機能追加

--- src/main/java/com/example/musespringapi/controller/FollowController.java
+++ src/main/java/com/example/musespringapi/controller/FollowController.java
@@ -13,9 +13,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.musespringapi.domain.FollowNotification;
 import com.example.musespringapi.domain.Relation;
 import com.example.musespringapi.domain.User;
 import com.example.musespringapi.response.followUserResponse;
+import com.example.musespringapi.service.NotificationService;
 import com.example.musespringapi.service.RelationService;
 
 import lombok.RequiredArgsConstructor;
@@ -23,22 +25,26 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 public class FollowController {
-	
+
 	private final RelationService relationService;
-	
-	
+	private final NotificationService notificationService;
+
 	@PostMapping("/follow")
 	@ResponseStatus(HttpStatus.CREATED)
 	public void insertFollowUser(@RequestBody Relation relation) {
-		System.out.println(relation.getFollowerUserNum());
-		System.out.println(relation.getFollowingUserNum());
-		
-	
-		
-			relationService.insertFollowUser(relation);
-		
+		// System.out.println(relation.getFollowerUserNum());
+		// System.out.println(relation.getFollowingUserNum());
+		FollowNotification followNotification = new FollowNotification();
+
+		followNotification.setFollowTransfer(relation.getFollowingUserNum());
+		followNotification.setFollowReceiver(relation.getFollowerUserNum());
+
+		relationService.insertFollowUser(relation);
+
+		notificationService.insertFollowNotification(followNotification);
+
 	}
-	
+
 //	@GetMapping("/unfollow/{followingUserNum}")
 //	public List<Relation> getFollowUser(@PathVariable("followingUserNum") String followingUserNum){
 //		return relationService.getFollowerUserNum(followingUserNum);
