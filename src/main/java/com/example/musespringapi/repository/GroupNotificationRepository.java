4c456199c36d6256b54f77e4d8897516c553ba99
tatsuki-Kasa <tatsuki.kasawaki@rakus-partners.co.jp>
2020-12-21 17:18:58 +0900

    Groupテーブルから情報持ってくる処理追加

--- src/main/java/com/example/musespringapi/repository/GroupNotificationRepository.java
+++ src/main/java/com/example/musespringapi/repository/GroupNotificationRepository.java
@@ -1,13 +1,17 @@
 package com.example.musespringapi.repository;
 
+import java.util.List;
+
 import com.example.musespringapi.domain.GroupNotification;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GroupNotificationRepository extends JpaRepository<GroupNotification, Integer> {
 
-
+    @Query(value = "SELECT * FROM group_notification WHERE group_receiver = ?1 ORDER BY group_notification_id DESC", nativeQuery = true)
+    List<GroupNotification> getGroupNotificationByReceiver(String receiver);
 
 }
