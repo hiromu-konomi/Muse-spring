2dffc3ed8910ee4bb2bb17a104bab853cd4f9e83
tatsuki-Kasa <tatsuki.kasawaki@rakus-partners.co.jp>
2020-12-21 17:23:59 +0900

    グループ、いいねのselect処理追加

--- src/main/java/com/example/musespringapi/service/NotificationService.java
+++ src/main/java/com/example/musespringapi/service/NotificationService.java
@@ -3,12 +3,18 @@ package com.example.musespringapi.service;
 import java.util.List;
 
 import com.example.musespringapi.domain.FollowNotification;
+import com.example.musespringapi.domain.Group;
 import com.example.musespringapi.domain.GroupNotification;
+import com.example.musespringapi.domain.LikeNotification;
 import com.example.musespringapi.domain.NotificationParents;
+import com.example.musespringapi.domain.Post;
 import com.example.musespringapi.domain.User;
 import com.example.musespringapi.repository.FollowNotificationRepository;
 import com.example.musespringapi.repository.GroupNotificationRepository;
+import com.example.musespringapi.repository.GroupRepository;
+import com.example.musespringapi.repository.LikeNotificationRepository;
 import com.example.musespringapi.repository.NotificationParentsRepository;
+import com.example.musespringapi.repository.PostRepository;
 import com.example.musespringapi.repository.UserRepository;
 
 import org.springframework.stereotype.Service;
@@ -22,16 +28,24 @@ public class NotificationService {
     private final NotificationParentsRepository notificationParentsRepository;
     private final FollowNotificationRepository followNotificationRepository;
     private final GroupNotificationRepository groupNotificationRepository;
+    private final LikeNotificationRepository likeNotificationRepository;
     private final UserRepository userRepository;
+    private final GroupRepository groupRepository;
+    private final PostRepository postRepository;
 
-    //** フォロー、フォロワー時通知を誰が受け取るのかのインポート */
+    //** フォロー、フォロワー時通知を誰が受け取るのかのインサート */
     public void insertFollowNotification(FollowNotification followNotification) {
         followNotificationRepository.save(followNotification);
     }
 
-    /** 誰がどのグループに招待したのかインポート */
+    /** 誰がどのグループに招待したのかインサート */
     public void insertGroupNotification(GroupNotification groupNotification) {
         groupNotificationRepository.save(groupNotification);
+    }
+
+    /** 誰がいいねを押したのかインサート */
+    public void insertLikeNotification(LikeNotification likeNotification) {
+        likeNotificationRepository.save(likeNotification);
     }
 
     //** フォロー受け取った人よりフォローした人のfollowNum,typeをselect */
@@ -39,8 +53,29 @@ public class NotificationService {
         return followNotificationRepository.getFollowNotificationByReceiver(followReceiver);
     }
 
-    public User getUserByFollowReceiver(String followTransfer){
-        return userRepository.userNameList(followTransfer);
+    //** いいねを受け取った人よりいいねした人のuserNumをセレクト */
+    public List<LikeNotification> getLikeNotificationListByReceiver(String likeReceiver){
+        return likeNotificationRepository.getLikeNotificationListByReceiver(likeReceiver);
+    }
+
+    /** グループ招待を受け取った人より招待した人のuserNum,groupIdをセレクト */
+    public List<GroupNotification> getGroupNotificationListByReceiver(String groupReceiver){
+        return groupNotificationRepository.getGroupNotificationByReceiver(groupReceiver);
+    }
+
+    //** 通知を渡したユーザー情報 */
+    public User getUserByTransfer(String transfer){
+        return userRepository.userNameList(transfer);
+    }
+
+    /** どのグループから招待が来たのか */
+    public Group getGroupByGroupId(Integer groupId){
+        return groupRepository.findById(groupId);
+    }
+
+    /** いいねを押されたuserNumをインサートする際に、postIdからReceiverUserNumを持ってくる */
+    public Post getPostByPostId(Integer postId) {
+        return postRepository.getPostTextUserIdByPostId(postId);
     }
 
     public NotificationParents getNotiPareByNotiPareId(Integer notiPareId){
