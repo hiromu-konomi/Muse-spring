2957776fe933b575c065750b45c581a46e3f1991
tatsuki-Kasa <tatsuki.kasawaki@rakus-partners.co.jp>
2020-12-22 16:26:11 +0900

    競合修正

--- src/main/java/com/example/musespringapi/controller/PostRestController.java
+++ src/main/java/com/example/musespringapi/controller/PostRestController.java
@@ -14,7 +14,6 @@ import com.example.musespringapi.domain.ShowReview;
 import com.example.musespringapi.domain.User;
 import com.example.musespringapi.response.CheckMusicResponse;
 import com.example.musespringapi.response.PostResponce;
-import com.example.musespringapi.response.ReviewResponce;
 import com.example.musespringapi.service.CheckService;
 import com.example.musespringapi.service.LikeService;
 import com.example.musespringapi.service.PostCardService;
@@ -41,14 +40,12 @@ public class PostRestController {
     private final PostService postService;
 
     private final PostCardService postCardService;
-   
-    private final UserService userService;
-    
-    private final LikeService likeService;
-    
-    private final CheckService checkService;
 
     private final UserService userService;
+
+    private final LikeService likeService;
+
+    private final CheckService checkService;
 
     @PostMapping("/form")
     @ResponseStatus(HttpStatus.CREATED)
@@ -83,11 +80,11 @@ public class PostRestController {
     public ResponseEntity<PostResponce> getMusicInfo(String userNum) {
         List<ShowReview> reviewList = new ArrayList<>();
         List<Post> postIdAllList = new ArrayList<>();
-      
+
         //自分の投稿取得
         List<Post> postIdList =postService.getPostIdFromFollowingUser(userNum);
         for(Post post : postIdList ) {
-        	postIdAllList.add(post);
+            postIdAllList.add(post);
         }
 
         for (Post post : postIdAllList) {
@@ -102,7 +99,7 @@ public class PostRestController {
             showReview.setPostText(post.getPostText());
             showReview.setPostId(post.getPostId());
             showReview.setUserNum(post.getUserNum());
-            
+
             Like like = likeService.userNumAndPostId(userNum, post.getPostId());
             if (like == null) {
                 showReview.setLikeStatus(false);
@@ -111,7 +108,7 @@ public class PostRestController {
             }
             Integer likeList = likeService.likeList(post.getPostId());
             showReview.setLikeCount(likeList);
-            
+
             Integer musicId = checkService.getMusicId(post.getPostId());
             Check check = checkService.checkList(userNum, musicId);
             if (check == null) {
@@ -122,7 +119,7 @@ public class PostRestController {
             // musicIdからチェックされた総数を数える
             Integer countCheck = checkService.checkCount(musicId);
             showReview.setCheckCount(countCheck);
-          
+
 
             reviewList.add(showReview);
         }
@@ -130,29 +127,27 @@ public class PostRestController {
         reviewList.sort(Comparator.comparing(ShowReview::getPostId).reversed());
 
         PostResponce postResponce = PostResponce.builder().reviewAllList(reviewList).build();
-       
+
         return new ResponseEntity<>(postResponce, HttpStatus.OK);
 
     }
 
     @GetMapping("/getLikePosts")
     public ResponseEntity<PostResponce> getLikePosts(String userNum) {
-    	System.out.println("ユーザーID＝"+ userNum);
+        System.out.println("ユーザーID＝"+ userNum);
         List<ShowReview> reviewList = new ArrayList<>();
-        
-         
+
         List<Post> postList = new ArrayList<Post>();
         //いいねした投稿のPostIdを取得
         List<Like> likeList = postService.getLikePosts(userNum);
-        
+
         if(likeList == null) {
-        	return null;
+            return null;
         } else {
-        	 for(Like like : likeList ) {
-            	 Post post = postService.getPostByPostId(like.getPostId());
-            	 postList.add(post);
+            for(Like like : likeList ) {
+                Post post = postService.getPostByPostId(like.getPostId());
+                postList.add(post);
             }
-            
             for (Post post : postList) {
                 ShowReview showReview = new ShowReview();
                 Music music = postCardService.findByPostId(post.getPostId());
@@ -164,7 +159,7 @@ public class PostRestController {
                 showReview.setPostText(post.getPostText());
                 showReview.setPostId(post.getPostId());
                 showReview.setUserNum(post.getUserNum());
-                
+
                 Like like = likeService.userNumAndPostId(userNum, post.getPostId());
                 if (like == null) {
                     showReview.setLikeStatus(false);
@@ -173,7 +168,7 @@ public class PostRestController {
                 }
                 Integer likeCount = likeService.likeList(post.getPostId());
                 showReview.setLikeCount(likeCount);
-                
+
                 Integer musicId = checkService.getMusicId(post.getPostId());
                 Check check = checkService.checkList(userNum, musicId);
                 if (check == null) {
@@ -184,70 +179,64 @@ public class PostRestController {
                 // musicIdからチェックされた総数を数える
                 Integer countCheck = checkService.checkCount(musicId);
                 showReview.setCheckCount(countCheck);
-               
+
                 reviewList.add(showReview);
             }
 
             reviewList.sort(Comparator.comparing(ShowReview::getPostId).reversed());
 
             PostResponce postResponce = PostResponce.builder().reviewAllList(reviewList).build();
-           
+
             return new ResponseEntity<>(postResponce, HttpStatus.OK);
         }
     }
-    
+
     @GetMapping("/getCheckSongs")
     public ResponseEntity<CheckMusicResponse> getCheckSongs(String userNum) {
-    	System.out.println("ユーザーID＝"+ userNum);
-        
-    	List<CheckMusic> checkMusicList = new ArrayList<CheckMusic>();
-        
+        System.out.println("ユーザーID＝"+ userNum);
+
+        List<CheckMusic> checkMusicList = new ArrayList<CheckMusic>();
         List<Music> musicList = new ArrayList<Music>();
         //チェックしたMusicIdを取得
         List<Check> checkList = checkService.getCheckSongsByUserNum(userNum);
-        
+
         if(checkList == null) {
-        	return null;
+            return null;
         } else {
-        	 for(Check check : checkList ) {
-            	 Music music = checkService.getMusicsByMusicId(check.getMusicId());
-            	 musicList.add(music);
+            for(Check check : checkList ) {
+                Music music = checkService.getMusicsByMusicId(check.getMusicId());
+                musicList.add(music);
             }
-            
+
             for (Music music : musicList) {
-                
+
                 CheckMusic checkMusic = new CheckMusic();
-               
+
                 checkMusic.setArtistName(music.getArtistName());
                 checkMusic.setMusicImage(music.getImage());
                 checkMusic.setMusicName(music.getMusicName());
                 checkMusic.setMusicId(music.getMusicId());
                 checkMusic.setPostId(music.getPostId());
-                
-          
+
                 Check check = checkService.checkList(userNum, music.getMusicId());
                 if (check == null) {
-                	checkMusic.setCheckStatus(false);            
+                    checkMusic.setCheckStatus(false);
                 } else {
                     checkMusic.setCheckStatus(true);
                 }
                 // musicIdからチェックされた総数を数える
                 Integer countCheck = checkService.checkCount(music.getMusicId());
                 checkMusic.setCheckCount(countCheck);
-               
+
                 checkMusicList.add(checkMusic);
             }
 
 //            reviewList.sort(Comparator.comparing(ShowReview::getPostId).reversed());
-            
             checkMusicList.sort(Comparator.comparing(CheckMusic::getPostId).reversed());
-           
-            
+
             CheckMusicResponse checkResponce = CheckMusicResponse.builder().checkAllList(checkMusicList).build();
-           
+
             return new ResponseEntity<>(checkResponce, HttpStatus.OK);
         }
     }
-    
-    
 }
